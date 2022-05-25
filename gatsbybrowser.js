730aa487d6a2858d99bed0ab98e80cc161122cb6
IsaacMorzy <musyokaisaac98@gmail.com>
2022-05-25 19:08:47 +0300

    tawk proto

---
+++ gatsbybrowser.js
@@ -0,0 +1,13 @@
+export const onInitialClientRender = () => {
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/628e37da7b967b1179912e26/1g3tokbn6';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    }
+
+
